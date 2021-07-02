c9b8dd7a385a3ee95ecf97f25230c79f3982207c
Triston <tony.lewis0398@gmail.com>
2021-07-02 10:28:32 -0400

    fix(printing): moved styles outline

--- src/views/Printing.vue
+++ src/views/Printing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full" style="height: 100vh; width: 100vw">
-    <div class="md:text-8xl font-bold">Big Printing Partners</div>
-    <div class="flex flex-col md:flex-row justify-evenly">
+    <div class="printingHeader">Big Printing Partners</div>
+    <div class="partnerTextContainer">
       <div id="partners">
         <img src="../../public/craftcloud.png" />
         <img src="../../public/shape.png" />
@@ -9,15 +9,13 @@
         <img src="../../public/imaterial.png" />
         <img src="../../public/xometry.png" />
       </div>
-      <div id="description" class="shadow-md  md:p-12 text-2xl md:w-1/2 text-left">
+      <div id="description" class="description">
         During your build process you have the option to choose from a list of
         top rated 3D printing services to turn your idea into a life size 3D
         puzzle! They will custom print your joint files and ship them to your
-        door.
-
-          In addition, our cut shop can read the cut sheet for all the sticks
-          you will need for your puzzle and pre-cut, label and ship them to you
-          as well
+        door. In addition, our cut shop can read the cut sheet for all the
+        sticks you will need for your puzzle and pre-cut, label and ship them to
+        you as well
       </div>
     </div>
   </div>
@@ -29,4 +27,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.printingHeader {
+  @apply md:text-8xl font-bold;
+}
+.partnerTextContainer {
+  @apply flex flex-col md:flex-row justify-evenly;
+}
+.description {
+  @apply shadow-md md:p-12 text-2xl md:w-1/2 text-left;
+}
+</style>
