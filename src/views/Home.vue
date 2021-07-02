60eaa02313a2f67298dd401291d21b0b2b91ace0
Triston <tony.lewis0398@gmail.com>
2021-07-02 10:19:03 -0400

    fix(home): moved all styles out of line

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <div style="width: 100vw; height: 100vh">
-    <div
-      id="nav"
-      class="
-        justify-between
-        flex
-        navbar
-        items-center
-        text-black
-
-        flex-col
-        md:flex-row
-      "
-    >
+    <div id="nav" class="navbar">
       <img src="../../public/big3dlogo.png" />
       <a @click="scrollMeTo('Home')">Home</a>
       <a @click="scrollMeTo('Story')">Story</a>
@@ -23,28 +11,24 @@
       <a @click="scrollMeTo('Purchase')">Purchase</a>
       <a @click="scrollMeTo('Login')">Login</a>
     </div>
-    <div ref="Home" class="home relative text-center">
-      <div class="image">
-        <img
-          style="width: 100%"
-          class="bg-opacity-5"
-          src="../../public/truckFrame.png"
-        />
+    <div ref="Home" class="home">
+      <div class="imageDeSaturation">
+        <img class="imgStyle" src="../../public/truckFrame.png" />
       </div>
-      <div class="md:text-9xl text-center centered font-bold">
+      <div class="homeHeader">
         <div class="flex justify-evenly">
-          <div class="white">think</div>
-          <div class="yellow">BIG</div>
+          <div class="text-white">think</div>
+          <div class="text-yellow">BIG</div>
         </div>
         <div class="flex justify-evenly">
-          <div class="white">print</div>
-          <div class="yellow">BIG</div>
+          <div class="text-white">print</div>
+          <div class="text-yellow">BIG</div>
         </div>
         <div class="flex justify-evenly">
-          <div class="white">build</div>
-          <div class="yellow">BIG</div>
+          <div class="text-white">build</div>
+          <div class="text-yellow">BIG</div>
         </div>
-        <div class="white md:text-3xl pt-12">
+        <div class="homeSubheader">
           Upload a low poly 3D model and generate a life size BIG 3D puzzle
         </div>
       </div>
@@ -52,7 +36,7 @@
 
     <a ref="Story"> <Story /></a>
     <a ref="Samples"> <Samples id="Samples" /> </a>
-<!--    <a ref="Gallery"> <Gallery id="Gallery" /> </a>-->
+    <!--    <a ref="Gallery"> <Gallery id="Gallery" /> </a>-->
     <a ref="Pricing"> <Pricing id="Pricing" /> </a>
     <a ref="Printing"> <Printing id="Printing" /> </a>
     <a ref="Purchase"> <Purchase id="Purchase" /> </a>
@@ -90,19 +74,36 @@ import { Options, Vue } from "vue-class-component";
 export default class Home extends Vue {}
 </script>
 <style>
-.yellow {
-  color: yellow;
-}
 .centered {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
-.white {
-  color: white;
-}
-.image {
+
+.imageDeSaturation {
   background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8));
+}
+.imgStyle {
+  @apply bg-opacity-5;
+  width: 100%;
+}
+.navbar {
+  @apply justify-between
+  flex
+
+  items-center
+  text-black
+  flex-col
+  md:flex-row;
+}
+.home {
+  @apply relative text-center;
+}
+.homeHeader {
+  @apply md:text-9xl text-center centered font-bold;
+}
+.homeSubheader {
+  @apply text-white md:text-3xl pt-12;
 }
 </style>
