023329ea1efd81eb33f7824ee3f14cd8dc6c230c
Miles Bond <miles.bond.biz@gmailmiles@milesbond.com>
2021-07-06 01:59:43 -0500

    Purchasing page - start
    creating rows

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,25 +1,61 @@
 <template>
-  <div class="relative w-full h-full" style="height: 100vh">
-    <div class="pricingHeader">What does it Cost?</div>
+  <div class="relative w-full h-full grid-rows-3">
+    <div class="pricingHeader">What does it Cost</div>
 
-    <div class="pricingCardContainer">
-      <div class="pricingCards">
+    <div class="pricingCardContainer grid grid-rows-1 gap-4">
+      <div class="pricingCards p-8">
         <div class="font-bold">DIY Route</div>
         <div>
           If you have your own 3D printer and are ready to print your own models
           then this is the route for you.
         </div>
-        <div class="font-bold">.50 per .STL joint generated</div>
+    <!-- DIY List -->        
+          <ul class="list-disc space-y-2 download-list justify-center object-center">
+            <li class="items-start download-list-item">
+              <span class="h-6 flex items-center sm:h-7">
+                <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <p class="ml-2">
+                .50 per .STL joint print.
+              </p>
+            </li>
+            <li class="flex items-start download-list-item">
+              <span class="h-6 flex items-center sm:h-7">
+                <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <p class="ml-2">
+                * zuo file of pre-supported .stl files.
+              </p>
+            </li>
+            <li class="flex items-start download-list-item">
+              <span class="h-6 flex items-center sm:h-7">
+                <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <p class="ml-2">
+              *Full cutest listing stick lengths to cut
+              </p>
+            </li>
+            <li class="flex items-start download-list-item">
+              <span class="h-6 flex items-center sm:h-7">
+                <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <p class="ml-2">
+              * simple instruction guide for assembly
+              </p>
+            </li>
+          </ul>
 
-        <div class="font-bold">
-          <div>whats included...</div>
-          <div>* zuo file of pre supported .stl files</div>
-          <div>* full cutest listing stick lengths to cut</div>
-          <div>* simple instruction guide for assembly</div>
-        </div>
       </div>
-      <div class="pricingCards">
-        Finished Frame Route
+      <div class="pricingCards p-8">
+        <div class="font-bold">Finished Frame Route</div>
 
         <div>Just send me the finished puzzle ready for assembly</div>
         <div class="font-bold">
@@ -76,6 +112,6 @@ export default {
 }
 
 .pricingCards {
-  @apply shadow-md rounded-md w-1/3 text-left;
+  @apply shadow-md rounded-md w-1/3 text-left bg-gray-100;;
 }
 </style>
