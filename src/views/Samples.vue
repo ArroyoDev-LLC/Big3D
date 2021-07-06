9592a20d1337539f3574ee70738f9b618db9d5cb
Miles Bond <miles.bond.biz@gmailmiles@milesbond.com>
2021-07-06 00:47:02 -0500

    Sample page changes
    Centering and coloring

--- src/views/Samples.vue
+++ src/views/Samples.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="relative w-full h-full p-8" style="height: 100vh">
-    <div class="font-bold text-2xl mb-6">
+  <div class="samples-page relative w-full h-full p-8" style="height: 100vh">
+    <div class="font-bold text-2xl mb-6 max-w-prose">
       Sometimes you just need to go <span>BIG</span>! Here are a few projects
       where a life size print was just too much fun to build
     </div>
-    <div class="flex flex-wrap mb-6">
-      Our original inspiration was a personal quest to build a lifesize Tesla
+    <div class="flex flex-wrap mb-6 justify-center">
+      <p class="max-w-prose">Our original inspiration was a personal quest to build a lifesize Tesla
       Cybertruck for all the fans who may need to wait a little longer to pull
       that beast in to there garage. But in the process of building the model,
       we stumbled on an even better idea... The ability to build AN ANYTHING at
       all kinds of scales .Here are a few examples of some really fun projects
-      build with big3D.me.
+      build with big3D.me.</p>
     </div>
 
-    <div class="flex justify-evenly">
+    <div class="flex justify-evenly sample-list">
       <div class="flex flex-col">
         <img src="../../public/sample1.png" />
         <div>African Elephant</div>
@@ -39,6 +39,15 @@ export default {
 
 <style scoped>
 span {
-  color: yellow;
+  color: #FBBF24;
 }
+
+.samples-page {
+  @apply bg-gray-200 w-5/6 md:w-full p-7 ;
+}
+
+.sample-list {
+  @apply bg-gray-50 w-5/6 md:w-full grid grid-cols-3 gap-4 border-t-2 border-b-2 p-7 ;
+}
+
 </style>
