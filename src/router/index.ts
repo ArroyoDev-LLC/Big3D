ce44c509223ad4a68e99586e2a1229dcdb9a7632
Triston Lewis <tony.lewis0398@gmail.com>
2021-07-20 10:35:38 -0500

    feat(new homepage): adding new route for home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
+import Home from "../views/NizaHome.vue";
 import Story from "../views/Story.vue";
 import Login from "../views/Login.vue";
 import Samples from "../views/Samples.vue";
@@ -9,57 +9,57 @@ import Printing from "../views/Printing.vue";
 import Purchase from "../views/Purchase.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  // {
-  //   path: "/story",
-  //   name: "story",
-  //   component: Story
-  // },
-  // {
-  //   path: "/samples",
-  //   name: "samples",
-  //   component: Samples
-  // },
-  // {
-  //   path: "/gallery",
-  //   name: "gallery",
-  //   component: Gallery
-  // }, {
-  //   path: "/pricing",
-  //   name: "pricing",
-  //   component: Pricing
-  // }, {
-  //   path: "/printing",
-  //   name: "printing",
-  //   component: Printing
-  // },
-  // {
-  //   path: "/purchase",
-  //   name: "purchase",
-  //   component: Purchase
-  // }, {
-  //   path: "/login",
-  //   name: "login",
-  //   component: Login
-  // }
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    // {
+    //   path: "/story",
+    //   name: "story",
+    //   component: Story
+    // },
+    // {
+    //   path: "/samples",
+    //   name: "samples",
+    //   component: Samples
+    // },
+    // {
+    //   path: "/gallery",
+    //   name: "gallery",
+    //   component: Gallery
+    // }, {
+    //   path: "/pricing",
+    //   name: "pricing",
+    //   component: Pricing
+    // }, {
+    //   path: "/printing",
+    //   name: "printing",
+    //   component: Printing
+    // },
+    // {
+    //   path: "/purchase",
+    //   name: "purchase",
+    //   component: Purchase
+    // }, {
+    //   path: "/login",
+    //   name: "login",
+    //   component: Login
+    // }
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 });
 
 export default router;
---
+++ src/views/NizaHome.vue
@@ -0,0 +1,13 @@
+<template>
+<div>test</div>
+</template>
+
+<script>
+export default {
+  name: "NizaHome"
+}
+</script>
+
+<style scoped>
+
+</style>
