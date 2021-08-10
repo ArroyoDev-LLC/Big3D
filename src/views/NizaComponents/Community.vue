c416093727beb591e1454ae58176087473638e40
Niza Valenzuela <niza.valenzuela.25@student.prcc.edu>
2021-08-10 16:20:45 -0500

    feat(community page)

---
+++ src/views/NizaComponents/Community.vue
@@ -0,0 +1,45 @@
+<template>
+<div class= "flex justify-evenly" id="Community">
+    <div class= "justify-center ml-8 text-left pt-60">
+        <text class= "text-sm">
+            COMMUNITY
+        </text>
+
+        <text class= "text-3xl font-bold text-big3dBlack pb-4"> 
+            <br>Our fans <br> featured <span class= "bg-yellow">works</span><br>
+        </text>
+
+        <text class= "text-xs">
+            Our community is growing every
+            day and you can follow it on <br> social networks.
+            To your attention the work of our clients, which <br> they shared with us!
+        </text>
+        
+    </div>
+
+    <div class="flex justify-evenly pt-20"> 
+        <div class= "flex flex-col"> 
+            <img class= "p-3" src= "Rectangle 38.png"/>
+            <img class= "p-3" src= "Rectangle 39.png"/>
+        </div>
+
+        <div class= "flex flex-col"> 
+            <img  class= "p-3" src= "Rectangle 40.png"/>
+            <img class= "p-3" src= "Rectangle 41.png"/>
+        </div>
+
+    </div>
+
+
+</div>
+</template>
+
+<script lang=ts>
+export default {
+  name: "Community"
+}
+</script>
+
+<style scoped lang="postcss">
+
+</style>
