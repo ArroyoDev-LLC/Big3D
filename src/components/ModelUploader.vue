47a1ffb95a932ec1fa7e08e4c331a2c4dcd66d63
Deep Panchal <deep302001@gmail.com>
2021-08-10 22:09:22 -0500

    feat(comp): add loading spinner, add prop types, add more props, set css layout

--- src/components/ModelUploader.vue
+++ src/components/ModelUploader.vue
@@ -7,18 +7,49 @@
         class="m-uploader__input"
         name="m-uploader"
         type="file"
+        :disabled="isLoading"
         @change="handleFileChange($event.target.files)"
       />
 
-      <div class="text-2xl font-bold">
-        {{ label }}
+      <div class="m-uploader__bg-image">
+        <img :src="bgImagePath" alt="m-uploader-bg-image" />
       </div>
 
-      <hr class="w-2/3 bg-white h-px m-8" />
+      <div v-if="isLoading" class="m-uploader__loader">
+        Uploading file {{ selectedModelName }}...
+        <svg
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
 
-      <button class="font-bold cursor-pointer py-4 px-8 bg-white text-black">
-        Browse
-      </button>
+      <div v-else class="m-uploader__labels">
+        <div class="md:text-2xl font-bold">
+          {{ label }}
+        </div>
+
+        <hr class="w-2/3 bg-white h-px m-8" />
+
+        <button class="font-bold cursor-pointer py-4 px-8 bg-white text-black">
+          Browse
+        </button>
+      </div>
     </div>
 
     <small v-show="errorMessage" class="text-red-500">{{ errorMessage }}</small>
@@ -26,16 +57,40 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
+import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
   name: "ModelUploader",
   components: {},
   props: {
-    id: String,
-    label: String,
-    subLabel: String,
-    errorMessage: String,
+    id: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    subLabel: {
+      type: String,
+      default: "",
+    },
+    errorMessage: {
+      type: String,
+      default: "",
+    },
+    bgImagePath: {
+      type: String,
+      default: "",
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    selectedModelName: {
+      type: String,
+      default: "",
+    },
     handleFileChange: {
       type: Function as PropType<(files: FileList) => Promise<unknown>>,
       default: null,
@@ -62,6 +117,7 @@ export default defineComponent({
 }
 
 .m-uploader__input {
+  z-index: 10;
   @apply absolute
   top-0
   left-0
@@ -69,5 +125,17 @@ export default defineComponent({
   w-full
   h-full
   cursor-pointer;
+}
+
+.m-uploader__bg-image {
+  @apply absolute;
+  z-index: 0;
+  bottom: 2%;
+  right: 1%;
+}
+
+.m-uploader__loader {
+  @apply absolute inset-0 flex flex-col items-center;
+  top: 40%;
 }
 </style>
