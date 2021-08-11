ab6e7df27a1285f44923fca05a917beda580ce3f
pablo014 <angelopablo014@gmail.com>
2021-08-11 16:02:09 -0600

    feat(graphics): add conditional rendering and total calculatiosn

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -11,13 +11,14 @@
       >
         <text class="col-span-auto -ml-4 sm:0">Choose Delivery</text>
         <text class="col-span-auto text-sm sm:text-lg -ml-6 sm:ml-0"
-          ><span class="text-yellow">54</span> Connectors</text
+          ><span class="text-yellow">{{ numConnectors }}</span> Connectors</text
         >
         <text class="col-span-auto text-sm sm:text-lg"
-          ><span class="text-yellow">112</span> Edges</text
+          ><span class="text-yellow">{{ numEdges }}</span> Edges</text
         >
       </div>
       <div
+        :class="deliveryOption === 'Finished KIT' ? 'opacity-40' : ''"
         class="
           row-span-1
           border-b border-dashed border-opacity-30
@@ -37,11 +38,14 @@
           <img src="connectors.png" />
         </div>
         <div class="col-span-1 text-right flex flex-col">
-          <text class="font-bold">$28.08</text>
+          <text class="font-bold">{{
+            formatToDollar(numConnectors * 0.52)
+          }}</text>
           <text class="text-yellow">(.52 per connector)</text>
         </div>
       </div>
       <div
+        :class="deliveryOption === 'DIY Route' ? 'opacity-40' : ''"
         class="
           row-span-1
           border-b border-dashed border-opacity-30
@@ -61,7 +65,7 @@
           <img src="parcel.png" />
         </div>
         <div class="col-span-1 text-right flex flex-col">
-          <text class="font-bold">$135.00</text>
+          <text class="font-bold">{{ formatToDollar(numEdges * 0.52) }}</text>
           <text class="text-yellow">(2.50 per connector)</text>
         </div>
       </div>
@@ -100,10 +104,27 @@ import RadioButton from "primevue/radiobutton";
 export default defineComponent({
   name: "Delivery",
   components: { RadioButton },
+  props: {
+    numConnectors: {
+      type: Number,
+      default: 0,
+    },
+    numEdges: {
+      type: Number,
+      default: 0,
+    },
+  },
   setup() {
     const deliveryOption = ref();
+    const formatToDollar = (dollarAmount: number) => {
+      return dollarAmount.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    };
     return {
       deliveryOption,
+      formatToDollar,
     };
   },
 });
