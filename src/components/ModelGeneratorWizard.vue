7d9e70d9106befab16305363db955f807d0e1c96
Deep Panchal <deep302001@gmail.com>
2021-08-11 17:55:29 -0500

    refactor(comp/wizard): add comment docs, refactor code, format code

--- src/components/ModelGeneratorWizard.vue
+++ src/components/ModelGeneratorWizard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="wizard">
-    <div class="wizard__current" :class="`order-${activeStep + 1}`">
+    <div :class="`order-${activeStep + 1}`" class="wizard__current">
+      <!-- ---------------------------------- -->
+      <!-- --------- Upload Step --------------->
+      <!-- ---------------------------------- -->
       <div
         v-show="currentStep.name === WizardSteps.UPLOAD"
         class="p-4 md:p-8 flex flex-col items-center h-full w-full"
@@ -14,22 +17,22 @@
           </div>
           <div class="grid grid-cols-6 gap-4 items-center col-span-2">
             <input
-              class="col-span-4 text-black p-4"
-              type="number"
-              min="1"
-              max="100"
-              step="0.01"
-              name="longest-dimension-input"
               id="longest-dimension-input"
               v-model="longestDimension"
               :disabled="isLoading"
+              class="col-span-4 text-black p-4"
+              max="100"
+              min="1"
+              name="longest-dimension-input"
+              step="0.01"
+              type="number"
             />
             <select
-              class="col-span-2 bg-white p-4 text-black"
-              name="unit-selector"
               id="unit-selector"
               v-model="longestDimensionUnit"
               :disabled="isLoading"
+              class="col-span-2 bg-white p-4 text-black"
+              name="unit-selector"
             >
               <option
                 v-for="(unit, index) in unitOptions"
@@ -42,29 +45,37 @@
           </div>
         </div>
         <ModelUploader
-          class="w-full md:w-5/6 h-full m-3"
-          label="Drag’n’drop your model here"
-          accept=".stl,.blend"
-          bg-image-path="images/elephant-model.png"
+          :class="disabledClasses"
+          :disabled="isLoading || !isLongestDimensionValid"
+          :error-message="null"
+          :handle-file-change="handleModelUpload"
           :is-loading="isLoading"
           :selected-model-name="isLoading ? modelFile.name : null"
-          :handle-file-change="handleModelUpload"
-          :error-message="null"
-          :disabled="isLoading || !isLongestDimensionValid"
-          :class="disabledClasses"
+          accept=".stl,.blend"
+          bg-image-path="images/elephant-model.png"
+          class="w-full md:w-5/6 h-full m-3"
+          label="Drag’n’drop your model here"
         />
         <NextStepButton
           v-if="nextStep && modelFile"
-          class="self-end"
           :class="disabledClasses"
-          :label="nextStep.label"
           :disabled="isLoading"
+          :label="nextStep.label"
+          class="self-end"
           @click="setStep(activeStep + 1)"
         />
       </div>
+
+      <!-- ---------------------------------- -->
+      <!-- --------- Dimensions Step ----------->
+      <!-- ---------------------------------- -->
       <div v-show="currentStep.name === WizardSteps.DIMENSIONS">
         {{ currentStep.label }}
       </div>
+
+      <!-- ---------------------------------- -->
+      <!-- --------- Connectors Step ----------->
+      <!-- ---------------------------------- -->
       <div
         v-show="currentStep.name === WizardSteps.CONNECTORS"
         class="p-4 md:p-8 h-full w-full"
@@ -72,15 +83,23 @@
         <ConnectorView
           :connector-info="connectorInfo"
           :title="currentStep.title"
-          @radioChange="handleConnecterInput"
+          @radioChange="handleConnectorInput"
         />
       </div>
+
+      <!-- ---------------------------------- -->
+      <!-- --------- Delivery Step ------------->
+      <!-- ---------------------------------- -->
       <div v-show="currentStep.name === WizardSteps.DELIVERY">
         <Delivery
           class="w-full h-full m-3"
           @toCheckout="setStep(activeStep + 1)"
         />
       </div>
+
+      <!-- ---------------------------------- -->
+      <!-- --------- Checkout Step ------------->
+      <!-- ---------------------------------- -->
       <div v-show="currentStep.name === WizardSteps.CHECKOUT">
         {{ currentStep.label }}
       </div>
@@ -90,8 +109,8 @@
     <div
       v-for="(step, index) in steps"
       :key="`${step.name}-${index}`"
-      class="wizard__step"
       :class="`order-${getStepOrder(step.name)}`"
+      class="wizard__step"
       @click="setStep(step.name)"
     >
       <div>{{ step.label }}</div>
@@ -168,14 +187,18 @@ export default defineComponent({
     const currentStep = computed(() => steps[activeStep.value]);
     const nextStep = computed(() => steps[activeStep.value + 1] || null);
 
+    /**
+     * state management for loading + disabled inputs
+     */
     const isLoading = ref<boolean>(false);
     const disabledClasses = computed(() => ({
       "cursor-not-allowed": isLoading.value,
       "opacity-30": isLoading.value,
     }));
 
-    const modelFile = ref<File | null>(null);
-
+    /**
+     * helpers for manipulating steps
+     */
     const getStep = (step: number) => steps[step];
     const setStep = (step: number) => (activeStep.value = step);
     const enableStep = (step: number) => (steps[step].isDisabled = false);
@@ -183,14 +206,16 @@ export default defineComponent({
 
     const getStepOrder = (step: WizardSteps) => step + 1;
 
+    /**
+     * handle state for file upload step
+     * and validate dimensions
+     */
+    const modelFile = ref<File | null>(null);
     const handleModelUpload = async (files: FileList) => {
       console.log(files);
       modelFile.value = files[0];
       isLoading.value = true;
-
       await new Promise((r) => setTimeout(r, 5000));
-
-      console.log(files);
       isLoading.value = false;
     };
 
@@ -199,6 +224,10 @@ export default defineComponent({
     const longestDimensionUnit = ref("ft");
     const isLongestDimensionValid = computed(() => longestDimension.value > 0);
 
+    /**
+     * prevent user from entering dimensions
+     * greater than or less than specified value
+     */
     watchEffect(() => {
       const min = 1;
       const max = 100;
@@ -209,16 +238,15 @@ export default defineComponent({
       }
     });
 
-    watchEffect(() => {
-      console.log(!isLongestDimensionValid.value);
-    });
-
+    /**
+     * handle state for connector step
+     */
     const connectorInfo = reactive({
       connectors: 0,
       edges: 0,
     });
 
-    const handleConnecterInput = (selection: string) => console.log(selection);
+    const handleConnectorInput = (selection: string) => console.log(selection);
 
     return {
       WizardSteps,
@@ -240,7 +268,7 @@ export default defineComponent({
       enableStep,
       disableStep,
       handleModelUpload,
-      handleConnecterInput,
+      handleConnectorInput,
     };
   },
 });
@@ -275,6 +303,6 @@ export default defineComponent({
 .wizard__current {
   grid-column: span 13 / span 13;
   @apply text-white
-    flex justify-center items-center;
+  flex justify-center items-center;
 }
 </style>
