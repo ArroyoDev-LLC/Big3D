d32e96568fc13c7587f32466ad711b9e1ea1edf3
Triston <Weasel0398@gmail.com>
2021-08-12 15:00:00 -0500

    added toast to main ts

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,10 @@ import Dialog from "primevue/dialog";
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+import ToastService from "primevue/toastservice";
 
 createApp(App)
+  .use(ToastService)
   .use(PrimeVue)
   .component("Dialog", Dialog)
   .use(store)
