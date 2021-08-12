e06a352b9ea7c3239ba50b6ba73af03ba25ce7a2
Triston <Weasel0398@gmail.com>
2021-08-12 18:05:57 -0500

    added order object for ordercard

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="order-card">
     <div class="w-1/3 h-1/3 p-6">person and project</div>
-    <div class="text-2xl text-yellow font-bold p-2">{{ fileName }}</div>
-    <div class="text-white">Last Downloaded on {{ date }} @ {{ time }}</div>
+    <div class="text-2xl text-yellow font-bold p-2">
+      {{ order.fileName }}
+    </div>
+    <div class="text-white m-1">
+      Last Downloaded on {{ order.date }} @ {{ order.time }}
+    </div>
     <Button class="bg-white p-4 m-8">Download File</Button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
   name: "OrderCard",
-  setup() {
-    const fileName = ref("Name of file");
-    return {
-      fileName,
-    };
+  props: {
+    order: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
