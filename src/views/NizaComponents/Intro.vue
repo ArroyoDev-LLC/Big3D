d550443768cf41f17afbe98cfb41126317d024dc
Triston <Weasel0398@gmail.com>
2021-09-08 13:40:54 -0500

    adding stripe payments

--- src/views/NizaComponents/Intro.vue
+++ src/views/NizaComponents/Intro.vue
@@ -7,24 +7,60 @@
 
       <text>
         Time for big toys! Generate full-size 3D model any form <br />
-        you wantand get constructor model of your dreams
+        you want and get constructor model of your dreams
       </text>
       <GoBigButton class="my-5 ml-10 sm:ml-0" />
+
+
+      <stripe-checkout
+        ref="checkoutRef"
+        mode="payment"
+        :pk="publishableKey"
+        :line-items="lineItems"
+        :success-url="successURL"
+        :cancel-url="cancelURL"
+        @loading="(v) => (loading = v)"
+      />
+      <button @click="submit">Pay now!</button>
+      0
     </div>
 
     <div>
       <img src="Frame 1.png" />
     </div>
+
   </div>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from "vue";
 import GoBigButton from "@/components/GoBigButton.vue";
+import { StripeCheckout } from "@vue-stripe/vue-stripe";
 
 export default defineComponent({
   name: "Intro",
-  components: { GoBigButton },
+  components: { GoBigButton, StripeCheckout },
+  data() {
+    return {
+      publishableKey: "pk_test_51JXTC3LEeW8HHB6ICHZKK8JqmrcxEw2Gv1guXlT7uJNrONda0xeRU2bZi5KYz3jVO6CR6TVUlqeyjC1rfDbJ8PIv00sZSIonWF",
+      loading: false,
+      lineItems: [
+        {
+          price: "price_1JXUiZLEeW8HHB6I43nlyTOB", // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1
+        }
+      ],
+      successURL: "https://localhost:8080/#/",
+      cancelURL: "https://localhost:8080/#/"
+    };
+  },
+  methods: {
+    submit() {
+      // You will be redirected to Stripe's secure checkout page
+      this.$refs.checkoutRef.redirectToCheckout();
+    }
+  }
 });
 </script>
 
