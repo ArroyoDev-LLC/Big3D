6799c0e0cae7c2e536e177f41061029e1c7e34de
Triston <Weasel0398@gmail.com>
2021-09-10 10:15:32 -0500

    intro page and samples page fixing spacing and layout issues

--- src/views/NizaComponents/Printing.vue
+++ src/views/NizaComponents/Printing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 text-left mx-10 mb-5">
-    <div class="flex flex-col justify-center col-span-1">
+    <div class="flex flex-col lg:justify-center col-span-1">
       <text class="text-base">OUR COLLABORATORS</text>
       <br />
       <text class="text-2xl font-black text-black">Big printing partners</text>
@@ -14,26 +14,26 @@
           <br />
           In addition, our cut shop can read the cut sheet for all the sticks
           you will need foryour puzzle and pre-cut, label and ship them to you
-          as well.</text
-        >
+          as well.
+        </text>
       </div>
     </div>
 
     <div class="flex flex-col justify-evenly mt-4 mb-4 col-span-1">
       <div class="flex">
-        <div class="flex flex-col">
-          <img class="m-1" src="Frame 14.png" width="600" height="100" />
-          <img class="m-1" src="Frame 15.png" width="600" height="100" />
+        <div class="flex flex-col p-4">
+          <img class="m-1 p-1" src="Frame 14.png" width="600" height="100" />
+          <img class="m-1 p-1" src="Frame 15.png" width="600" height="100" />
         </div>
 
-        <div class="flex flex-col">
-          <img class="m-1" src="Frame 16.png" width="600" height="100" />
-          <img class="m-1" src="Frame 17.png" width="600" height="100" />
+        <div class="flex flex-col p-4">
+          <img class="m-1 p-1" src="Frame 16.png" width="600" height="100" />
+          <img class="m-1 p-1" src="Frame 17.png" width="600" height="100" />
         </div>
       </div>
 
       <div>
-        <img class="m-1 w-full" src="Frame 18.png" width="600" height="100" />
+        <img class="m-1 w-full h-1/2 p-5" src="Frame 18.png" />
       </div>
       <!--<img :key="idx + item" v-for=" idx, item in imgArray" src='https://picsum.photos/200/165' />-->
     </div>
