1ac4ac3da20cb30865106d1cc550cc859c73ca98
pablo014 <angelopablo014@gmail.com>
2022-07-20 09:26:31 -0600

    feat(tailwind): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,23 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   theme: {
-    colors: {
-      big3dBlack: "#1E1E1A",
-      big3dGray: "#ECECEC",
-      yellow: "#F6FB00",
-      gold: "#FBBF24",
-      white: "#ffffff",
-      black: "#000000",
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
       serif: ["Merriweather", "serif"],
     },
     extend: {
+      colors: {
+        big3dBlack: "#1E1E1A",
+        big3dGray: "#ECECEC",
+        yellow: "#F6FB00",
+        gold: "#FBBF24",
+        white: "#ffffff",
+        black: "#000000",
+        gray: colors.coolGray,
+        blue: colors.lightBlue,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
       outline: {
         black: "2px solid #000000",
         white: "2px solid #ffffff",
@@ -27,6 +27,7 @@ module.exports = {
       spacing: {
         128: "32rem",
         144: "36rem",
+        160: "48rem",
       },
       borderRadius: {
         "4xl": "2rem",
