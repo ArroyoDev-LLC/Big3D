25e610b63586c1f50cec44334f4b3574ed1bd758
Deep Panchal <deep302001@gmail.com>
2022-07-21 14:57:49 -0500

    test: update e2e tests

--- cypress/e2e/basic.spec.ts
+++ cypress/e2e/basic.spec.ts
@@ -3,34 +3,8 @@ context('Basic', () => {
     cy.visit('/')
   })
 
-  it('basic nav', () => {
-    cy.url()
-      .should('eq', 'http://localhost:3333/')
-
-    cy.contains('[Home Layout]')
-      .should('exist')
-
-    cy.get('#input')
-      .type('Vitesse{Enter}')
-      .url()
-      .should('eq', 'http://localhost:3333/hi/Vitesse')
-
-    cy.contains('[Default Layout]')
-      .should('exist')
-
-    cy.get('[btn]')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3333/')
-  })
-
-  it('markdown', () => {
-    cy.get('[title="About"]')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3333/about')
-
-    cy.get('pre.language-js')
-      .should('exist')
+  it('basic', () => {
+    const url = 'http://localhost:3333/'
+    cy.url().should('eq', url)
   })
 })
