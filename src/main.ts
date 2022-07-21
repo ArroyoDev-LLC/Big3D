5d2888d37bee0167eeea60772987140006b51008
Deep Panchal <deep302001@gmail.com>
2022-07-21 09:01:25 -0500

    feat!: replace all files with @antfu/vitesse template
    - https://github.com/antfu/vitesse/tree/a3059fb887c641a3ee6ee2b2f9bbb49ba7611961

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "@/assets/css/tailwind.css";
-import PrimeVue from "primevue/config";
-import Dialog from "primevue/dialog";
+import { ViteSSG } from 'vite-ssg'
+import { setupLayouts } from 'virtual:generated-layouts'
+import App from './App.vue'
+import type { UserModule } from './types'
+import generatedRoutes from '~pages'
 
-import "primevue/resources/themes/saga-blue/theme.css"; //theme
-import "primevue/resources/primevue.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
-import ToastService from "primevue/toastservice";
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
 
-createApp(App)
-  .use(ToastService)
-  .use(PrimeVue)
-  .component("Dialog", Dialog)
-  .use(store)
-  .use(router)
-  .mount("#app");
+const routes = setupLayouts(generatedRoutes)
+
+// https://github.com/antfu/vite-ssg
+export const createApp = ViteSSG(
+  App,
+  { routes, base: import.meta.env.BASE_URL },
+  (ctx) => {
+    // install all modules under `modules/`
+    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+      .forEach(i => i.install?.(ctx))
+  },
+)
