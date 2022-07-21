688b88e5f23b2fb51ee6506fbf51192390d77a6d
Deep Panchal <deep302001@gmail.com>
2022-07-21 09:01:25 -0500

    feat!: replace all files with @antfu/vitesse template
    - https://github.com/antfu/vitesse/tree/a3059fb887c641a3ee6ee2b2f9bbb49ba7611961

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,26 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Vitesse',
+  meta: [
+    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
+</script>
+
 <template>
   <RouterView />
 </template>
-<Script>
-
-
-
-export default {
-  components: {  }
-}
-
-</Script>
-<style>
-html,
-body {
-  height: 100%;
-  scroll-behavior: smooth;
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-bottom: 40px;
-}
-.navbar {
-  background: #c4c4c4;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,33 @@
-<template>
-  <footer
-    class="
-      h-24
-      w-full
-      mt-6
-      bg-big3dBlack
-      text-white
-      flex
-      items-center
-      justify-center
-    "
-  >
-    &copy; 2021 BIG3D. All Rights Reserved
-  </footer>
-</template>
+<script setup lang="ts">
+const { t, availableLocales, locale } = useI18n()
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Footer",
-});
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
 </script>
 
-<style lang="postcss" scoped></style>
+<template>
+  <nav text-xl mt-6>
+    <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
+      <div i-carbon-campsite />
+    </RouterLink>
+
+    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </button>
+
+    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
+      <div i-carbon-language />
+    </a>
+
+    <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+      <div i-carbon-dicom-overlay />
+    </RouterLink>
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+      <div i-carbon-logo-github />
+    </a>
+  </nav>
+</template>
