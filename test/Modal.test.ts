a87e47ca24b7a71791279bc6b313a9bdf714e172
pablo014 <angelopablo014@gmail.com>
2022-07-29 09:26:47 -0600

    test(modal): add tests

---
+++ test/Modal.test.ts
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import Modal from '../src/components/Modal.vue'
+
+describe('Modal', () => {
+  it('should render', () => {
+    const wrapper = mount(Modal)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('should render header slot', () => {
+    const wrapper = mount(Modal, {
+      slots: { header: '<div id="header">header</div>' }
+    })
+    expect(wrapper.find('#header').text()).toBe('header')
+  })
+  it('should render body slot', () => {
+    const wrapper = mount(Modal, {
+      slots: { body: '<div id="body">body</div>' }
+    })
+    expect(wrapper.find('#body').text()).toBe('body')
+  })
+  it('should render footer slot', () => {
+    const wrapper = mount(Modal, {
+      slots: { footer: '<div id="footer">footer</div>' }
+    })
+    expect(wrapper.find('#footer').text()).toBe('footer')
+  })
+})
